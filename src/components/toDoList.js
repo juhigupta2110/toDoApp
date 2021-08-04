032475642d951c2b256cf4ab5243e6d9d589d986
juhigupta2110 <87846760+juhigupta2110@users.noreply.github.com>
2021-08-04 09:36:46 +0530

    Add files via upload

---
+++ src/components/toDoList.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {View, Text, TouchableOpacity} from 'react-native';
+
+const ToDoList = ({todos, toggleTodo}) => (
+  <View>
+    {todos.map(todo => (
+      <TouchableOpacity key={todo.id} onPress={() => toggleTodo(todo.id)}>
+        <Text
+          style={{
+            fontSize: 24,
+            textDecorationLine: todo.completed ? 'line-through' : 'none',
+          }}>
+          {todo.text}
+        </Text>
+      </TouchableOpacity>
+    ))}
+  </View>
+);
+
+export default ToDoList;
